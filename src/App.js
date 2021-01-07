d51cb3d024e5587b6ef871d414fe13c01f3d763f
kikicom <kihwan0624@hotmail.com>
2021-01-08 00:23:12 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import logo from './logo.svg';
+import React, {Component}  from 'react';
 import './App.css';
+import Customer from './components/Customer'
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management sysstem!ddd</h2>
-    </div>
-  );
+const customers=[{
+  id : 1,
+  image:'https://placeimg.com/64/64/any',
+  name : '홍길동',
+  birthday:'11111',
+  gender:'남자',
+  job:'테스터1'
+},
+{
+  id : 2,
+  image:'https://placeimg.com/64/64/any',
+  name : '이수빈',
+  birthday:'222222',
+  gender:'남자',
+  job:'테스터2'
+},
+{
+  id : 3,
+  image:'https://placeimg.com/64/64/any',
+  name : '이나리',
+  birthday:'333333',
+  gender:'여자',
+  job:'테스터4'
+}]
+class App extends Component {
+  render(){
+    return(
+      <div>
+        {
+          customers.map(c => {
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+                />
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
  
 export default App;
