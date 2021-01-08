f68ae3a0aacafa0c07c5b703b5b1dc15ccdb6981
kikicom <kihwan0624@hotmail.com>
2021-01-08 13:01:28 +0900

    Add Progress

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import TableBody  from '@material-ui/core/TableBody';
 import TableRow   from '@material-ui/core/TableRow';
 import TableCell  from '@material-ui/core/TableCell';
 import {withStyles} from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 
 const styles = theme => ({
   root : {
@@ -17,15 +19,20 @@ const styles = theme => ({
   },
   table : {
     minWidth : 1080
+  },
+  progress:{
+    margin : theme.spacing.unit * 2
   }
 })
 
 class App extends Component {
   state = {
-    customers : ""
+    customers : "",
+    completed : 0
   }
 
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
     .then(res => this.setState({customers : res}))
     .catch(err => console.log(err));
@@ -37,6 +44,10 @@ class App extends Component {
     return body;
   }
 
+  progress = () => {
+    const {completed}  = this.state;
+    this.setState({completed : completed >= 100 ? 0 : completed + 1}); 
+  }
 
   render(){
     const {classes} = this.props;
@@ -66,7 +77,12 @@ class App extends Component {
                 job={c.job}
                 />
             )
-          }) : ""
+          }) : 
+          <TableRow>
+            <TableCell colspan="6" align="center">
+              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+            </TableCell>
+          </TableRow>
         }     
           </TableBody>
         </Table>
