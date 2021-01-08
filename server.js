c378d08dcd6bf106180cacbf036d429c90d68b82
kikicom <kihwan0624@hotmail.com>
2021-01-08 17:17:41 +0900

    Create Customer Database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,36 +7,25 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// app.get('/api/hello', (req, res) => {
-//     res.send({message : 'Hello Express!'});
-// });
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host : conf.host,
+  user : conf.user,
+  password:conf.password,
+  port:conf.port,
+  database: conf.database
+});
+connection.connect();
 
 app.get('/api/customers',(req, res) => {
-    res.send([
-        {
-        id : 1,
-        image:'https://placeimg.com/64/64/any',
-        name : '홍길동',
-        birthday:'11111',
-        gender:'남자',
-        job:'테스터1'
-      },
-      {
-        id : 2,
-        image:'https://placeimg.com/64/64/any',
-        name : '이수빈',
-        birthday:'222222',
-        gender:'남자',
-        job:'테스터2'
-      },
-      {
-        id : 3,
-        image:'https://placeimg.com/64/64/any',
-        name : '이나리',
-        birthday:'333333',
-        gender:'여자',
-        job:'테스터4'
-      }
-    ]);
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) =>{
+      res.send(rows);
+    }
+  );
 })
 app.listen(port, () => console.log(`Listening on port ${port}`));
